54538ff85e0212c0954e454d2052a11bce296605
Akshara Gopinathan <akshara.g@itp.objectfrontier.com>
2018-03-16 15:16:24 +0530

    Updated controller

--- src/js/controller/myctrl.js
+++ src/js/controller/myctrl.js
@@ -16,80 +16,72 @@ function myCtrl(restCalls,$scope,$window){
 
 	$scope.initleft = function(){
 			$(function () {
-					var getData;
-		 			var tree=[];
+				var getData;
+		 		var tree=[];
 		 			
-      				restCalls.getFlattenTree()
-         				.then(function successCallBack(response){
-           							for(var i=0;i<response.data.length;i++){
-											var temp={};
-											if(response.data[i].parentid==0){
-												temp.id=response.data[i].id;
-												temp.parent='#';
-												temp.text=response.data[i].name;
-												tree.push(temp)
-												}else{
-													temp.id=response.data[i].id;
-													temp.parent=response.data[i].parentid;
-													temp.text=response.data[i].name;
-													tree.push(temp);
-												}
-											}
+      			restCalls.getFlattenTree()
+         			.then(function successCallBack(response){
+           				for(var i=0;i<response.data.length;i++){
+								var temp={};
+								if(response.data[i].parentid==0){
+									temp.id=response.data[i].id;
+									temp.parent='#';
+									temp.text=response.data[i].name;
+									tree.push(temp)
+									}else{
+										temp.id=response.data[i].id;
+										temp.parent=response.data[i].parentid;
+										temp.text=response.data[i].name;
+										tree.push(temp);
+										}
+						}
 											createJSTree(tree);
-									})
-							// });
-			
-           			
-        		});
+					})
+			});
 
-        function createJSTree(tree) {            
-            $('#left').jstree({
-                'core': {
-                'data': tree
-              		},
-              	"check_callback" : true,
-              	'plugins':[ "contextmenu","wholerow","types","dnd","state"],
-              	'contextmenu':{
-              		"select_node": true, 
+       		function createJSTree(tree) {            
+            	$('#left').jstree({
+                	'core': {
+                		'data': tree
+              			},
+              		"check_callback" : true,
+              		'plugins':[ "contextmenu","wholerow","types","dnd","state"],
+              		'contextmenu':{
+              			"select_node": true, 
               			"items": function ($node) {
                        		var trees = $("#left").jstree(true);
                         	return {
                         			"Remove": {
-									            "separator_before": false,
-									            "separator_after": false,
-									            "label": "Remove",
-									            "action": function (obj) {
-									            	
-									                    var id = $node.id;
-									                   restCalls.deleteHostNode(id)
+									        "separator_before": false,
+									        "separator_after": false,
+									        "label": "Remove",
+									        "action": function (obj) {
+									           		var id = $node.id;
+									                 restCalls.deleteHostNode(id)
          												.then(function successCallBack(response){
 									        					location.reload();					
 																});					           
-									                                 
-									                        }
+														  }
 									            },
                           			"Create": {
-                            	 				"separator_before": false,
-          										"separator_after": false,
-           										"label": "Create",
-           										"action": function (obj) { 
-           											var id=$node.id;
+                            	 			"separator_before": false,
+          									"separator_after": false,
+           									"label": "Create",
+           									"action": function (obj) { 
+           										var id=$node.id;
            											$scope.$apply(function(){
            												$scope.hostgroup.id="";
            												$scope.hostgroup.name="";
            												$scope.hostgroup.description="";
-           												$scope.hostgroup.hostbaseline="";
-           												$scope.hostgroup.suppress_excluded_service="";
-           												$scope.hostgroup.send_to_cta="";
-           												$scope.hostgroup.host_trap="";
-           												$scope.hostgroup.inverse_suppression="";
+           												$scope.hostgroup.hostbaseline="false";
+           												$scope.hostgroup.suppress_excluded_service="false";
+           												$scope.hostgroup.send_to_cta="false";
+           												$scope.hostgroup.host_trap="false";
+           												$scope.hostgroup.inverse_suppression="false";
            												$scope.hostgroup.parentid=id;
-           											
-           											})
-           											
-           											
-            														}
-                           						}
+           												})
+           											}
+                           					}
 									};
 								}
 							}
@@ -117,7 +109,7 @@ function myCtrl(restCalls,$scope,$window){
 
 						$scope.initleft();
 						$scope.submit =function(id,hostgroup){
-			if(id==null){
+			if(id==null||id==""){
 			
 			restCalls.submitNewForm(hostgroup).then(function successCallBack(response){
 				$window.alert("Successfully Submitted");
@@ -125,11 +117,12 @@ function myCtrl(restCalls,$scope,$window){
 				console.log("success")
 			});
 		}else{
-			
+
 			restCalls.submit(id,hostgroup).then(function successCallBack(response){
 				$window.alert("Updated Successfully");
 				location.reload();
 			})
+		
 
 		}
 }
